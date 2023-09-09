39da02d90f3cfefddfd919fecf51683765113757
KinMiu <115400530+KinMiu@users.noreply.github.com>
2023-09-09 17:31:50 +0700

    update default

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.static('statics'))
-router.use('/', (req, res) => {
+router.use('/default', (req, res) => {
     res.json({
         msg: 'Welcome to API'
     });
